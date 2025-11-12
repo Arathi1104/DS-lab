686a30470fd0e0dc4866cc852aefdcf3dea07a38
Arathi1104 <arathik2711@gmail.com>
2025-11-12 12:30:50 +0530

    Add files via upload

--- list.c
+++ list.c
@@ -167,8 +167,8 @@ int main()
 {
    do
 	{
-		int	item,choice;
-		printf("\n1.insertAtFront\n2.insertAtEnd\n3.insertAtAny\n4.deleteAtFirst\n5.deleteAtFirst\n6.deleteAtEnd\n7.deleteAtAny\n8.Exit");
+		int	item,choice,key;
+		printf("\n1.insertAtFront\n2.insertAtEnd\n3.insertAtAny\n4.deleteAtFirst\n5.deleteAtEnd\n6.deleteAtAny\n7.display\n8.Exit\n");
 		printf("Enter your choice:");
 		scanf("%d",&choice);
 		switch(choice)
@@ -179,27 +179,26 @@ int main()
 				   break;
 			case 2:printf("Enter the element:");
 				   scanf("%d",&item);
-				   insertAtFront(item);
+				   insertAtEnd(item);
 				   break;
-			case 3:display();
+			case 3:printf("Enter the element:");
+				   scanf("%d",&item);
+				   printf("Enter the key before which to insert: ");
+           scanf("%d", &key);
+				   insertAtAny(item,key);
 				   break;
-			case 4:return(0);
+			case 4:deleteAtFirst();
+				   break;
+			case 5:deleteAtEnd();
+				   break;
+			case 6:deleteAtAny(key);
+				   break;
+			case 7:traverselist();
+				   break;
+			case 8:return(0);
 			default:printf("Enter	the	correct	choice.");
 							break;														
 		}
 	}while(1);
-    insertAtFront(10);
-    insertAtFront(20);
-    insertAtFront(30);
-    insertAtFront(40);
-    insertAtEnd(50);
-    insertAtEnd(60);
-    insertAtAny(25, 30);   
-    insertAtAny(80, 10);   
-    deleteAtFirst();
-    deleteAtEnd();
-    deleteAtAny(30);
-    traverselist();
-
-    return 0;
+return(0);
 }
