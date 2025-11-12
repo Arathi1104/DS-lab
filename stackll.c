49f124036f38b19b92f1c3be1b6f9efb787d1953
Arathi1104 <arathik2711@gmail.com>
2025-11-12 12:03:28 +0530

    Add files via upload

--- stackll.c
+++ stackll.c
@@ -8,8 +8,8 @@ struct stack
     int data;
     struct stack *link;
 };
-
 struct stack *top = NULL;
+void push(int item)
 {
     struct stack *newnode;
     newnode = (struct stack *)malloc(sizeof(struct stack));
@@ -47,12 +47,18 @@ void pop()
         ptr = top;
         top= top->link;
         free(ptr);
+        printf("successfully deleted.\n");
     }
 }
 void display()
 {
     struct stack *ptr;
     ptr = top;
+    if(ptr==NULL)
+    {
+    	printf("stack is empty.\n");
+    }
+    printf("stack elements:\n");
     while (ptr != NULL)
     {
         printf("%d\n", ptr->data);
@@ -61,9 +67,27 @@ void display()
 }
 int main()
 {
-	push(43);
-	push(32);
-	display();
+	do
+	{
+		int	item,choice;
+		printf("\n1.Push\n2.Pop\n3.Display\n4.Exit\n");
+		printf("Enter	your	choice:	");
+		scanf("%d",&choice);
+		switch(choice)
+		{
+			case	1:printf("Enter	the	element:");
+							scanf("%d",&item);
+							push(item);
+							break;
+			case	2:pop();
+							break;
+			case	3:display();
+							break;
+			case	4:return(0);
+			default:printf("Enter	the	correct	choice.");
+							break;														
+		}
+	}while(1);
 	
 return(0);
 }
